01d6da1a41afd2d66ee2a5e6faec2d483bf64c86
Dhanuka Perera <perera.dhanuka@gmail.com>
2025-03-09 16:06:15 +1100

    added port 8080

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080; // This is the crucial change
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World from Node.js and TypeScript!');
